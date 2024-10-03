734addf82d558b2213607e75a57c8d0c3a137c79
TiagoMendesSilva <tiago_mendessilva@yahoo.com>
2024-10-03 16:33:29 -0300

    feat: Add annotation @Transactional in the method from service

--- src/main/java/com/customer/customer/services/CustomerService.java
+++ src/main/java/com/customer/customer/services/CustomerService.java
@@ -6,6 +6,7 @@ import com.customer.customer.repositories.CustomerRepository;
 import com.customer.customer.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -17,17 +18,20 @@ public class CustomerService {
     @Autowired
     private CustomerRepository repository;
 
+    @Transactional(readOnly = true)
     public List<CustomerDTO> findAll(){
         List<Customer> customers = repository.findAll();
         return customers.stream().map(CustomerDTO::new).collect(Collectors.toList());
     }
 
+    @Transactional(readOnly = true)
     public CustomerDTO findById(Long id){
         Optional<Customer> obj = repository.findById(id);
         Customer customer = obj.orElseThrow(() -> new ResourceNotFoundException("Customer not found"));
         return new CustomerDTO(customer);
     }
 
+    @Transactional
     public CustomerDTO insert (CustomerDTO dto){
         Customer entity = new Customer();
         entity.setName(dto.getName());
