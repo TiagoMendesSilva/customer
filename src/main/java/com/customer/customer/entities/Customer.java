27f7f834d5983bc04cf0ea2eb5c7c59473bdedbb
TiagoMendesSilva <tiago_mendessilva@yahoo.com>
2024-10-07 21:00:23 -0300

    feat: Auditing data

--- src/main/java/com/customer/customer/entities/Customer.java
+++ src/main/java/com/customer/customer/entities/Customer.java
@@ -21,6 +21,12 @@ public class Customer implements Serializable {
     private Instant birthDate;
     private Integer children;
 
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
+
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
+
     public Customer() {
     }
 
@@ -80,6 +86,25 @@ public class Customer implements Serializable {
     public void setChildren(Integer children) {
         this.children = children;
     }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    public void prePersist(){
+        createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        updatedAt = Instant.now();
+    }
+
 
     @Override
     public boolean equals(Object o) {
